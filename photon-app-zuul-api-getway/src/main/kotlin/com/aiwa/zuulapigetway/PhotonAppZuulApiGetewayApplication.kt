9812d918f4471aa85d336b1eefa969d61ecf90b9
leonel47 <waboleonel@gmail.com>
2021-03-19 22:55:42 -0400

    Initial commit, Zuul Api geteway

---
+++ photon-app-zuul-api-getway/src/main/kotlin/com/aiwa/zuulapigetway/PhotonAppZuulApiGetewayApplication.kt
@@ -0,0 +1,11 @@
+package com.aiwa.zuulapigetway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotonAppZuulApiGetewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhotonAppZuulApiGetewayApplication>(*args)
+}
---
+++ photon-app-zuul-api-getway/src/test/kotlin/com/aiwa/zuulapigetway/PhotonAppZuulApiGetwayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aiwa.zuulapigetway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotonAppZuulApiGetwayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
