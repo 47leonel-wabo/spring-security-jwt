56e0469e60a8cc09361e6cd618f55565357c29fa
leonel47 <waboleonel@gmail.com>
2021-03-20 00:31:04 -0400

    Configuring Zuul Api gateway

--- photon-app-zuul-api-getway/src/main/kotlin/com/aiwa/zuulapigetway/PhotonAppZuulApiGetewayApplication.kt
+++ photon-app-zuul-api-getway/src/main/kotlin/com/aiwa/zuulapigetway/PhotonAppZuulApiGetewayApplication.kt
@@ -2,8 +2,12 @@ package com.aiwa.zuulapigetway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 class PhotonAppZuulApiGetewayApplication
 
 fun main(args: Array<String>) {
