3db33989c7552ccff6385658f8d3a03ef8037970
leonel47 <waboleonel@gmail.com>
2021-03-20 13:35:22 -0400

    Getting user listening port

--- photon-app-users-api/src/main/kotlin/com/aiwa/photon/app/users/controller/UserController.kt
+++ photon-app-users-api/src/main/kotlin/com/aiwa/photon/app/users/controller/UserController.kt
@@ -1,14 +1,16 @@
 package com.aiwa.photon.app.users.controller
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 
 @RestController
 @RequestMapping(path = ["/users"])
-class UserController {
+class UserController(val env: Environment) {
 
     @GetMapping(path = ["/check/status"])
-    fun appStatus(): String = "User service up and running..."
+    fun appStatus(): String = "User service up and running on ${env.getProperty("local.server.port")}"
 
 }
